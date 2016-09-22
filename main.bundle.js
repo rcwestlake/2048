540f993f2e7f2a2445e28dfbbec6ca7ec0ca8bd8
Ryan Westlake <ryancwestlake@gmail.com>
2016-09-22 12:48:18 -0600

    npm run build

--- main.bundle.js
+++ main.bundle.js
@@ -10139,44 +10139,264 @@
 
 /***/ },
 /* 2 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
-	function Board(row, column) {
-	  this.row = row || 4;
-	  this.column = column || 4;
-	  this.cells = [];
+	var $ = __webpack_require__(1);
+	var rowFor = __webpack_require__(3);
+	var columnFor = __webpack_require__(4);
+
+	var score = 0;
+	var highScore = 0;
+
+	function setUpGame() {
+	  var board = new Board();
+	  var game = new Game();
+	  board.addValueToAvailableCell.bind(board)();
+	  board.addValueToAvailableCell.bind(board)();
+	  board.render.bind(board)();
+	  document.addEventListener('keydown', board.handleKey.bind(board, game));
+	  renderHighScore();
+	  resetScore();
 	}
 
-	Board.prototype.buildGrid = function (x, y) {
-	  for (var i = 0; i < x; i++) {
-	    for (var j = 0; j < y; j++) {
+	$(document).ready(function () {
+	  setUpGame();
+	});
 
-	      this.cells.push({ x: i, y: j, value: null });
+	$(".new-game-button").on("click", function () {
+	  location.reload();
+	});
+
+	function Board() {
+	  this.row = 4;
+	  this.column = 4;
+	  this.cells = [];
+	  this.buildGrid(arguments);
+	}
+
+	function Cell(row, column, value) {
+	  this.row = row;
+	  this.column = column;
+	  this.value = value;
+	}
+
+	function Game() {
+	  this.cells = [];
+	  this.score = score;
+	  this.highScore = highScore;
+	}
+
+	Board.prototype = {
+
+	  addValueToAvailableCell: function () {
+	    var available = this.checkAvailalbeCells();
+	    var index = Math.floor(Math.random() * available.length);
+	    return available[index].value = Math.random() < 0.9 ? 2 : 4;
+	  },
+
+	  buildGrid: function (cells) {
+	    if (this.cells.length) {
+	      return this.cells;
 	    }
+	    for (var i = 0; i < 16; i++) {
+	      var value = cells[i] ? cells[i] : 0;
+	      this.cells.push(new Cell(rowFor(i), columnFor(i), value));
+	    }
+	  },
+
+	  checkAndChangeValue: function (direction) {
+	    var slice;
+	    var directionBoolean;
+
+	    if (direction === 1 || direction === 3) {
+	      slice = [[this.cells[0], this.cells[1], this.cells[2], this.cells[3]], [this.cells[4], this.cells[5], this.cells[6], this.cells[7]], [this.cells[8], this.cells[9], this.cells[10], this.cells[11]], [this.cells[12], this.cells[13], this.cells[14], this.cells[15]]];
+	    } else {
+	      slice = [[this.cells[0], this.cells[4], this.cells[8], this.cells[12]], [this.cells[1], this.cells[5], this.cells[9], this.cells[13]], [this.cells[2], this.cells[6], this.cells[10], this.cells[14]], [this.cells[3], this.cells[7], this.cells[11], this.cells[15]]];
+	    }
+
+	    if (direction === 1) {
+	      directionBoolean = true;
+	    } else if (direction === 2) {
+	      directionBoolean = true;
+	    } else if (direction === 3) {
+	      directionBoolean = false;
+	    } else {
+	      directionBoolean = false;
+	    }
+
+	    for (var i = 0; i < 4; i++) {
+	      for (var j = directionBoolean ? 0 : 3; directionBoolean ? j < 4 : j >= 0; directionBoolean ? j++ : j--) {
+	        slice[i][j].update(directionBoolean, slice[i], j);
+	      }
+	    }
+	  },
+
+	  checkAvailalbeCells: function () {
+	    var available = this.cells.filter(function (item) {
+	      return item.value === 0;
+	    });
+	    return available;
+	  },
+
+	  handleKey: function (game, event) {
+	    var moved = false;
+
+	    switch (event.keyCode) {
+	      case 37: // Left
+	      case 38: // Up
+	      case 39: // Right
+	      case 40:
+	        // Down
+	        event.preventDefault();
+	        break;
+	    }
+
+	    switch (event.keyCode) {
+	      case 37:
+	        // Left
+	        moved = this.processMove(1);
+	        break;
+	      case 38:
+	        // Up
+	        moved = this.processMove(2);
+	        break;
+	      case 39:
+	        // Right
+	        moved = this.processMove(3);
+	        break;
+	      case 40:
+	        // Down
+	        moved = this.processMove(4);
+	        break;
+	    }
+	    if (moved) {
+	      this.addValueToAvailableCell();
+	      this.render();
+	    }
+
+	    if (moved === false && this.checkAvailalbeCells().length === 0) {
+	      lose();
+	    }
+	  },
+
+	  printBoard: function () {
+	    for (var i = 0; i < 16; i++) {}
+	  },
+
+	  processMove: function (direction) {
+
+	    var moved = true;
+	    var previousState = this.toString(this.cells);
+	    this.checkAndChangeValue(direction);
+	    var currentState = this.toString(this.cells);
+	    if (previousState === currentState) {
+	      moved = false;
+	    }
+	    localStorage.setItem('state', currentState);
+	    localStorage.setItem('score', score);
+	    return moved;
+	  },
+
+	  render: function () {
+	    for (var i = 0; i < this.cells.length; i++) {
+	      var tile = document.getElementById(i);
+	      tile.innerHTML = this.cells[i].value;
+	      tile.className = `tile${ this.cells[i].value }`;
+	    }
+	  },
+
+	  toString: function (value) {
+	    return JSON.stringify(value);
 	  }
-	  return this.cells;
 	};
 
-	Board.prototype.createNewGrid = function () {
-	  return this.buildGrid(4, 4);
+	Cell.prototype = {
+
+	  update: function (direction, slice, positionInSlice) {
+	    for (var i = positionInSlice; direction ? ++i < 4 : --i >= 0;) {
+	      if (this.value === slice[i].value && this.value !== 0) {
+	        this.value = this.value * 2;
+	        slice[i].value = 0;
+	        this.calculateScore(this.value);
+	        this.setHighScore();
+	        return;
+	      } else if (this.value === 0 && slice[i].value > 0) {
+	        this.value = slice[i].value;
+	        slice[i].value = 0;
+	      } else if (slice[i].value > 0) {
+	        return;
+	      } else {
+	        continue;
+	      }
+	    }
+	  },
+
+	  calculateScore: function (value) {
+	    score += value;
+	    $('.current-score').text(score);
+	  },
+
+	  setHighScore: function () {
+	    var highScore = getHighScore();
+	    if (score >= highScore) {
+	      highScore = score;
+	    }
+	    localStorage.setItem('highScore', highScore);
+	    renderHighScore();
+	  }
 	};
 
-	Board.prototype.changeValue = function (index) {
-	  ++this.cells[index].value;
+	function lose() {
+	  alert('You lost. Play again!');
+	}
+
+	function getHighScore() {
+	  var storedHighScore = JSON.parse(localStorage.getItem('highScore'));
+	  if (storedHighScore) {
+	    highScore = storedHighScore;
+	  }
+	  return highScore;
+	}
+
+	function renderHighScore() {
+	  var highScore = getHighScore();
+	  $('.high-score').text(highScore);
+	}
+
+	function resetScore() {
+	  score = 0;
+	  $('.current-score').text(score);
+	  localStorage.setItem('score', score);
+	}
+
+	module.exports = [Board, Cell];
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	module.exports = function (number) {
+	  if (number < 4) {
+	    return 0;
+	  }
+	  if (number < 8) {
+	    return 1;
+	  }
+	  if (number < 12) {
+	    return 2;
+	  }
+	  if (number < 16) {
+	    return 3;
+	  }
 	};
 
-	Board.prototype.randomNumber = function () {
-	  return Math.floor(Math.random * cells.length); //write the rest of this
-	};
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
 
-	Board.prototype.checkAvailalbeCells = function () {
-	  var availabe = this.cells.filter(function (item, index, array) {
-	    return item.value === null;
-	  });
-	  return availabe;
+	module.exports = function (number) {
+	  return number % 4;
 	};
-
-	module.exports = Board;
 
 /***/ }
 /******/ ]);
